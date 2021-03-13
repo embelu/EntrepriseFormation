36db115cd02cc2fc07c08b951284e725d7467cce
Ludovic Baens <ludovic.baens@solidaris.be>
2021-03-13 19:31:04 +0100

    Add Entreprise + Entreprise.console

---
+++ FormationEntreprise/Entreprise/Interfaces/ITravailleur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entreprise.Interfaces
+{
+    public interface ITravailleur
+    {
+        int Id { get; set; }
+        string Nom { get; set; }
+        string Prenom { get; set; }
+        int Age { get; set; }
+
+        int Salaire();
+    }
+}
