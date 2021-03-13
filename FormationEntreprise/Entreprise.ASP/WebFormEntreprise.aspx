75d4451b82a501c1568a40f835d6c79fa226ba28
Ludovic Baens <ludovic.baens@solidaris.be>
2021-03-13 19:41:46 +0100

    Add Project Entreprise.ASP

---
+++ FormationEntreprise/Entreprise.ASP/WebFormEntreprise.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormEntreprise.aspx.cs" Inherits="Entreprise.ASP.WebFormEntreprise" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TBoxMatricule" runat="server" OnTextChanged="TBoxMatricule_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="BtnEnvoyer" runat="server" OnClick="BtnEnvoyer_Click" Text="Envoyer" />
+            <br />
+            <br />
+            <asp:Label ID="LblMatricule" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FormationEntreprise/Entreprise.ASP/WebFormEntreprise.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Entreprise.ASP
+{
+    public partial class WebFormEntreprise : System.Web.UI.Page
+    {
+        private Societe societe = new Societe();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                TBoxMatricule.Text = "Matricule";
+            }
+        }
+
+        protected void BtnEnvoyer_Click(object sender, EventArgs e)
+        {
+            LblMatricule.Text = TBoxMatricule.Text;
+        }
+
+        protected void TBoxMatricule_TextChanged(object sender, EventArgs e)
+        {
+            societe.Matricule = Int32.Parse(TBoxMatricule.Text);
+        }
+    }
+}
